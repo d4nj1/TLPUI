bb6868e895597953929ceee9d41a8a4183c69e79
Daniel Christophis <code@devmind.org>
2015-12-23 23:55:19 +0100

    * Unit test changes

--- unitests.py
+++ unitests.py
@@ -12,11 +12,17 @@ def get_config_count(categories):
         configcount += len(configs);
     return configcount
 
+
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        # tlp file config
+    def test_config_categories(self):
         configfilelist = read_tlp_file_config("/etc/default/tlp")
         configfilecategories = create_tlp_ui_categories(configfilelist)
+
+        self.assertEqual(len(configfilecategories), 12)
+
+    def test_tlp_file_vs_json_config(self):
+        # tlp file config
+        configfilelist = read_tlp_file_config("/etc/default/tlp")
 
         # json config
         jsonfile = open('configcategories.json')
@@ -24,7 +30,6 @@ class MyTestCase(unittest.TestCase):
         jsoncategories = jsonobject['categories']
         jsonconfigcount = get_config_count(jsoncategories)
 
-        self.assertEqual(len(configfilecategories), 12)
         self.assertEqual(len(configfilelist), 80)
         self.assertEqual(len(configfilelist), jsonconfigcount)
 
