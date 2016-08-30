5a942aaeaa0c60cd547e395e91a70a90188481e2
Daniel <code@devmind.org>
2016-08-30 16:34:47 +0200

    * Config available check added

--- tlpui.py
+++ tlpui.py
@@ -2,6 +2,7 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk
 
+from os import path
 from css import get_css_provider
 from file import read_tlp_file_config
 from mainui import create_main_box, close_window
@@ -13,11 +14,19 @@ Gtk.StyleContext.add_provider_for_screen(
 )
 
 defaultconfigpath = '/etc/default/tlp'
-tlpconfig = read_tlp_file_config(defaultconfigpath)
 
 window = Gtk.Window()
 window.set_default_size(900, 600)
-window.add(create_main_box(window, defaultconfigpath, tlpconfig))
+
+if path.exists(defaultconfigpath):
+    tlpconfig = read_tlp_file_config(defaultconfigpath)
+    window.add(create_main_box(window, defaultconfigpath, tlpconfig))
+else:
+    noconfiglabel = Gtk.Label("No config file found at " + defaultconfigpath)
+    noconfigbox = Gtk.Box()
+    noconfigbox.pack_start(noconfiglabel, True, True, 0)
+    window.add(noconfigbox)
+
 window.connect('delete-event', Gtk.main_quit)
 window.connect('key-press-event', close_window)
 window.show_all()
