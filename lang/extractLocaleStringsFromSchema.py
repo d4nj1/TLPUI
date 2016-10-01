e9c664862205df6199daab848146d5e864c9e77f
Daniel <code@devmind.org>
2016-10-01 21:24:48 +0200

    Issue #11: * Documentation added to helper method

--- lang/extractLocaleStringsFromSchema.py
+++ lang/extractLocaleStringsFromSchema.py
@@ -4,12 +4,17 @@ from file import get_json_schema_object_from_file
 
 
 def create_translateable_strings_header_file():
+    """ This method extracts items for translation from configschema.json and puts them in new file with
+    header extension (.h) because poedit and other po-tools do not support JSON files right now. This method
+    should be called any time configschema.json changes and translation needs to be updated.
+    Change configschema.json -> run this script -> update po files from source with utility (e.g. poedit)"""
+
     categories = get_json_schema_object_from_file('categories', '../configschema.json')
     newfile = open('configschema.json.h', 'w+')
 
     for category in categories:
-        configs = category['configs']
         newfile.write('_(\"' + category['name'] + '_CATEGORY\");\n')
+        configs = category['configs']
         for config in configs:
             if 'group' in config:
                 descriptiontag = config['group'] + '_GROUP'
