a0fc6cd97af58dee549cf10ad2c01f995297105f
Daniel <code@devmind.org>
2018-05-03 19:56:23 +0200

    Issue #28 * Setuptools build script updated

--- setup.py
+++ setup.py
@@ -5,5 +5,19 @@ setup(
     version="0.1",
     author="Daniel Christophis",
     author_email="code@devmind.org",
-    py_modules=["tlpui"]
+    description="GTK UI for tlp (Python 3)",
+    url="https://github.com/d4nj1/TLPUI",
+    license="GPLv2",
+    packages=["tlpui", "tlpui.ui_config_objects"],
+    package_data={'tlpui': [
+        'styles.css',
+        'configschema/*.json',
+        'icons/*.svg',
+        'lang/*/LC_MESSAGES/*.mo'
+    ]},
+    entry_points={
+        'gui_scripts': [
+            'tlpui = tlpui.__main__:main'
+        ]
+    }
 )
