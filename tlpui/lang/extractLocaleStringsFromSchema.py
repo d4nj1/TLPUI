0d7999e4e1ba4b1f4aa652fc6def2475286e1fca
Daniel <code@devmind.org>
2020-12-23 12:02:57 +0100

    #79 * Switched back to older version of translatable string extraction helper * Initial translation validation helper added

--- tlpui/lang/extractLocaleStringsFromSchema.py
+++ tlpui/lang/extractLocaleStringsFromSchema.py
@@ -1,33 +1,9 @@
 from io import open
-import os
 from json import load
+import os
 
 
 lastvalue = ''
-custom_translations = {
-    'TLP_PERSISTENT_DEFAULT':       ['1_0', '1_1'],         # ID  / since 1.0
-    'CPU_SCALING_GOVERNOR':         ['all'],                # GRP
-    'CPU_SCALING_FREQ':             ['all'],                # GRP
-    'CPU_HWP':                      ['1_0', '1_1'],         # GRP / since 1.0
-    'ENERGY_PERF_POLICY':           ['all'],                # GRP
-    'DISK_APM_LEVEL':               ['all'],                # GRP
-    'DISK_IOSCHED':                 ['all'],                # ID
-    'SATA_LINKPWR':                 ['all'],                # GRP
-    'SATA_LINKPWR_BLACKLIST':       ['0_9', '1_0', '1_1'],  # ID  / since 0.9
-    'AHCI_RUNTIME_PM':              ['0_9', '1_0', '1_1'],  # GRP / since 0.9
-    'AHCI_RUNTIME_PM_TIMEOUT':      ['0_9', '1_0', '1_1'],  # ID  / since 0.9
-    'WIFI_PWR':                     ['all'],                # GRP
-    'BAY_POWEROFF':                 ['1_0', '1_1'],         # GRP / since 1.0
-    'BAY_POWEROFF_ON_BAT':          ['0_8', '0_9'],         # ID  / until 0.9
-    'RUNTIME_PM_ALL':               ['0_8', '0_9'],         # ID  / until 0.9
-    'RUNTIME_PM_DRIVER_BLACKLIST':  ['all'],                # ID
-    'USB_BLACKLIST_BTUSB':          ['1_0', '1_1'],         # ID  / since 1.0
-    'USB_BLACKLIST_PHONE':          ['1_0', '1_1'],         # ID  / since 1.0
-    'USB_BLACKLIST_PRINTER':        ['1_1'],                # ID  / since 1.1
-    'USB_BLACKLIST_WWAN':           ['all'],                # ID
-    'RESTORE_THRESHOLDS_ON_BAT':    ['1_0', '1_1']          # ID  / since 1.0
-}
-
 
 def get_json_schema_object_from_file(objectname: str, filename: str) -> dict:
     jsonfile = open(filename)
@@ -60,38 +36,22 @@ def create_translateable_strings_header_file():
         if not file.endswith('.json'):
             continue
 
-        configversion = file.replace('.json', '')
-        versionfile = open('configschema.{}.json.h'.format(configversion), 'w+')
-
         categories = get_json_schema_object_from_file('categories', '../configschema/' + file)
+
         for category in categories:
             add_to_list(translateobjects, category['name'] + '__CATEGORY_TITLE')
             configs = category['configs']
             for config in configs:
                 if 'group' in config:
-                    if config['group'] in custom_translations.keys() \
-                            and any(v in custom_translations.get(config['group']) for v in {configversion, 'all'}):
-                        # versionfile.write('_(\"' + config['group'] + '__GROUP_TITLE' + '\");\n')
-                        versionfile.write('_(\"' + config['group'] + '__GROUP_DESCRIPTION' + '\");\n')
-                        # configitems = config['ids']
-                        # for configitem in configitems:
-                        #     versionfile.write('_(\"' + configitem['id'] + '__ID_TITLE' + '\");\n')
-                    elif config['group'] not in custom_translations.keys():
-                        # add_to_list(translateobjects, config['group'] + '__GROUP_TITLE')
-                        add_to_list(translateobjects, config['group'] + '__GROUP_DESCRIPTION')
-                        # configitems = config['ids']
-                        # for configitem in configitems:
-                        #     add_to_list(translateobjects, configitem['id'] + '__ID_TITLE')
+                    add_to_list(translateobjects, config['group'] + '__GROUP_TITLE')
+                    add_to_list(translateobjects, config['group'] + '__GROUP_DESCRIPTION')
+                    configitems = config['ids']
+                    for configitem in configitems:
+                        add_to_list(translateobjects, configitem['id'] + '__ID_TITLE')
                 else:
-                    if config['id'] in custom_translations.keys() \
-                            and any(v in custom_translations.get(config['id']) for v in {configversion, 'all'}):
-                        # versionfile.write('_(\"' + config['id'] + '__ID_TITLE' + '\");\n')
-                        versionfile.write('_(\"' + config['id'] + '__ID_DESCRIPTION' + '\");\n')
-                    elif config['id'] not in custom_translations.keys():
-                        # add_to_list(translateobjects, config['id'] + '__ID_TITLE')
-                        add_to_list(translateobjects, config['id'] + '__ID_DESCRIPTION')
+                    add_to_list(translateobjects, config['id'] + '__ID_TITLE')
+                    add_to_list(translateobjects, config['id'] + '__ID_DESCRIPTION')
 
-        versionfile.close()
 
     newfile = open('configschema.json.h', 'w+')
     for item in translateobjects:
