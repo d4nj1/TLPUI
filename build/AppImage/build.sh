427a21f11b3a31964fa007e9262e9932d6948bef
kronosol <45532845+blipk@users.noreply.github.com>
2021-03-25 12:13:21 +1100

    Update AppImage build script to download dependency

--- build/AppImage/build.sh
+++ build/AppImage/build.sh
@@ -13,10 +13,15 @@ mkdir TLPUI.AppDir/usr/share
 mkdir TLPUI.AppDir/usr/share/metainfo
 cp tlpui.appdata.xml TLPUI.AppDir/usr/share/metainfo/tlpui.appdata.xml
 
-#Run script
+# Copy run script
 cp AppRun TLPUI.AppDir/usr/bin/tlpui
 chmod +x TLPUI.AppDir/usr/bin/tlpui
 cp AppRun TLPUI.AppDir/AppRun
 chmod +x TLPUI.AppDir/AppRun
 
+# Download app image tool and use
+if [ ! -f appimagetool-x86_64.AppImage ]; then
+    wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage
+    chmod +x appimagetool-x86_64.AppImage
+fi
 ARCH=x86_64 ./appimagetool-x86_64.AppImage TLPUI.AppDir
