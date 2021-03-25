e4563af2583b31ff3c9598d1c8ecb1a0a03ac9cf
kronosol <45532845+blipk@users.noreply.github.com>
2021-03-25 22:52:55 +1100

    Improve AppImage file size

--- build/AppImage/pkg2appimage.sh
+++ build/AppImage/pkg2appimage.sh
@@ -5,5 +5,5 @@ if [ ! -f pkg2appimage-1801-x86_64.AppImage ]; then
     wget https://github.com/AppImage/pkg2appimage/releases/download/continuous/pkg2appimage-1801-x86_64.AppImage
     chmod +x pkg2appimage-1801-x86_64.AppImage
 fi
-VERSION=1 ARCH=x86_64 ./pkg2appimage-1801-x86_64.AppImage tlpui.yml
+VERSION_EXPANDED=1 ARCH=x86_64 ./pkg2appimage-1801-x86_64.AppImage tlpui.yml
 mv out/TLP_UI-.glibc2.32-x86_64.AppImage out/TLP_UI-x86_64.AppImage
