5d840f117a2707c655f07996ee5ac59d66f2fe47
kronosol <45532845+blipk@users.noreply.github.com>
2021-04-08 09:11:53 +1000

    Final

--- build/AppImage/pkg2appimage.sh
+++ build/AppImage/pkg2appimage.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-
-# Download app image tool and use
+# Download pkg2appimage image tool and use
 if [ ! -f pkg2appimage-1801-x86_64.AppImage ]; then
     wget https://github.com/AppImage/pkg2appimage/releases/download/continuous/pkg2appimage-1801-x86_64.AppImage
     chmod +x pkg2appimage-1801-x86_64.AppImage
 fi
-VERSION_EXPANDED=1 ARCH=x86_64 ./pkg2appimage-1801-x86_64.AppImage tlpui.yml
+ARCH=x86_64 ./pkg2appimage-1801-x86_64.AppImage tlpui.pkg.yml
 mv out/TLP_UI-.glibc2.32-x86_64.AppImage out/TLP_UI-x86_64.AppImage
