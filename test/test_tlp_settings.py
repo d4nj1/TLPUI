9219dd0d306e531b969ac97e48d2811193779bbc
Daniel <code@devmind.org>
2023-11-07 21:19:35 +0100

    #139 Switch to pytest

--- test/test_tlp_settings.py
+++ test/test_tlp_settings.py
@@ -1,41 +1,54 @@
-import unittest
+import pytest
 import linecache
 import shutil
-
 import os
-os.environ['XDG_CONFIG_HOME'] = os.path.dirname(os.path.abspath(__file__)) + '/settings/default'
 from tlpui import settingshelper
 
 
-class MyTestCase(unittest.TestCase):
+@pytest.fixture
+def with_broken_config(request):
+    settings_path = os.path.dirname(os.path.abspath(__file__)) + '/settings_default'
+    os.environ['XDG_CONFIG_HOME'] = settings_path
+    userconfig = settingshelper.UserConfig()
+    userconfig.userconfigfile.write_text("[default]")
+    userconfig.read_user_config()
 
-    def test_tlp_settings_broken(self):
-        os.environ['XDG_CONFIG_HOME'] = os.path.dirname(os.path.abspath(__file__)) + '/settings/default'
-        userconfig = settingshelper.UserConfig()
-        userconfig.userconfigfile.write_text("[default]")
-        userconfig.read_user_config()
-        assert linecache.getline(str(userconfig.userconfigfile), 1) == "[default]\n"
-        assert linecache.getline(str(userconfig.userconfigfile), 2) == "language = en_EN\n"
-        assert linecache.getline(str(userconfig.userconfigfile), 3) == "activeoption = 0\n"
-        assert linecache.getline(str(userconfig.userconfigfile), 4) == "activecategory = 0\n"
-        assert linecache.getline(str(userconfig.userconfigfile), 5) == "activeposition = 0\n"
-        assert linecache.getline(str(userconfig.userconfigfile), 6) == "windowxsize = 900\n"
-        assert linecache.getline(str(userconfig.userconfigfile), 7) == "windowysize = 600\n"
+    def clean_config_file():
+        shutil.rmtree(settings_path)
 
-    def test_tlp_settings_creation(self):
-        newsettingspath = os.path.dirname(os.path.abspath(__file__)) + '/settings/creation'
-        os.environ['XDG_CONFIG_HOME'] = newsettingspath
-        if os.path.exists(newsettingspath):
-            shutil.rmtree(newsettingspath)
-        userconfig = settingshelper.UserConfig()
-        assert linecache.getline(str(userconfig.userconfigfile), 1) == "[default]\n"
-        assert linecache.getline(str(userconfig.userconfigfile), 2) == "language = en_EN\n"
-        assert linecache.getline(str(userconfig.userconfigfile), 3) == "activeoption = 0\n"
-        assert linecache.getline(str(userconfig.userconfigfile), 4) == "activecategory = 0\n"
-        assert linecache.getline(str(userconfig.userconfigfile), 5) == "activeposition = 0\n"
-        assert linecache.getline(str(userconfig.userconfigfile), 6) == "windowxsize = 900\n"
-        assert linecache.getline(str(userconfig.userconfigfile), 7) == "windowysize = 600\n"
+    request.addfinalizer(clean_config_file)
+    return userconfig.userconfigfile
 
 
-if __name__ == '__main__':
-    unittest.main()
+@pytest.fixture
+def with_new_config(request):
+    settings_path = os.path.dirname(os.path.abspath(__file__)) + '/settings_creation'
+    os.environ['XDG_CONFIG_HOME'] = settings_path
+    userconfig = settingshelper.UserConfig()
+
+    def clean_config_file():
+        shutil.rmtree(settings_path)
+
+    request.addfinalizer(clean_config_file)
+    return userconfig.userconfigfile
+
+
+def test_tlp_settings_recreation(with_broken_config):
+    assert linecache.getline(str(with_broken_config), 1) == "[default]\n"
+    assert linecache.getline(str(with_broken_config), 2) == "language = en_EN\n"
+    assert linecache.getline(str(with_broken_config), 3) == "activeoption = 0\n"
+    assert linecache.getline(str(with_broken_config), 4) == "activecategory = 0\n"
+    assert linecache.getline(str(with_broken_config), 5) == "activeposition = 0\n"
+    assert linecache.getline(str(with_broken_config), 6) == "windowxsize = 900\n"
+    assert linecache.getline(str(with_broken_config), 7) == "windowysize = 600\n"
+
+
+def test_tlp_settings_creation(with_new_config):
+    assert linecache.getline(str(with_new_config), 1) == "[default]\n"
+    assert linecache.getline(str(with_new_config), 2) == "language = en_EN\n"
+    assert linecache.getline(str(with_new_config), 3) == "activeoption = 0\n"
+    assert linecache.getline(str(with_new_config), 4) == "activecategory = 0\n"
+    assert linecache.getline(str(with_new_config), 5) == "activeposition = 0\n"
+    assert linecache.getline(str(with_new_config), 6) == "windowxsize = 900\n"
+    assert linecache.getline(str(with_new_config), 7) == "windowysize = 600\n"
+
