f9b2148048b4600a0971df7ce3615f796113c9a8
Daniel <code@devmind.org>
2024-04-04 20:48:26 +0200

    #147 Fix loading css for GTK greater 4.12

--- tlpui/css.py
+++ tlpui/css.py
@@ -6,10 +6,7 @@ from . import settings
 
 def get_css_provider() -> Gtk.CssProvider:
     """Create css provider from stylesheet file and return it."""
-    with open(f'{settings.workdir}/styles.css', encoding='utf-8') as cssfile:
-        cssdata = cssfile.read()
-
     cssprovider = Gtk.CssProvider()
-    cssprovider.load_from_data(cssdata)
+    cssprovider.load_from_path(f'{settings.workdir}/styles.css')
 
     return cssprovider
